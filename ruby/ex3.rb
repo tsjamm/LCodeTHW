ea6766459c5121cf2f7591676c29cef78c3ca25d
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2014-08-01 15:29:15 +0530

    Ruby exercise 3

---
+++ ruby/ex3.rb
@@ -0,0 +1,18 @@
+puts "I will start counting cars"
+
+puts "BMW #{3+4/2}"
+puts "Mercedes #{5-2*2%2}"
+
+puts "now to count the bikes"
+
+puts 3+2+1-5+4%2-1/4+6
+
+puts "Is it true that 3+2 < 5-7 ?"
+puts 3+2<5-7
+
+puts "What is 3+2 ? #{3+2}"
+puts "What is 5-7 ? #{5-7}"
+
+puts "Now we know...."
+
+puts "#{5>-2} #{5>=-2} #{5<=-2}"
