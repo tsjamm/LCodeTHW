8ad67eb96393047dbb9dfbe46aeaa01834d078c5
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2014-08-01 17:23:00 +0530

    Ruby Exercise 5

---
+++ ruby/ex5.rb
@@ -0,0 +1,10 @@
+my_name = 'Sai Teja'
+my_age = 23
+my_hair = 'Black'
+
+puts "My name is #{my_name}"
+puts "My age is #{my_age}"
+puts "My hair is #{my_hair} in color"
+
+#Trying out simple addition
+puts "Lets add #{3} to my age #{my_age}, then we get #{my_age + 3}"
