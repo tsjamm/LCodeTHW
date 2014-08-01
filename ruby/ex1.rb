77f3be1208906948b0180e2d741c8d623ebf887f
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2014-08-01 14:49:19 +0530

    exercise 1 in ruby

---
+++ ruby/ex1.rb
@@ -0,0 +1,12 @@
+puts "Hello World of Ruby. I am on my way to master you..."
+puts "let's test out the ' ' symbols"
+puts 'and now lets do the "vice versa" version :P '
+puts "this preformated string by default
+
+is 
+
+very very
+            coooooooooooooool
+
+
+."
